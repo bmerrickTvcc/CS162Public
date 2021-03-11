813a6df3695c3f2bfdee57b7a7df2ee8b4ec3c94
Brian Merrick <BrianMerrick@boisestate.edu>
2021-03-11 11:14:50 -0700

    Adding BubbleSort starter code.

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // TODO: Read in Tests\bubbleSort.txt into an array
+
+            // TODO: Use the BubbleSort algorithm to sort the array
+        }
+    }
+}
