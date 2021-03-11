9051f415ca909108fe5720f7dc1d75b4e3524b5c
Brian Merrick <BrianMerrick@boisestate.edu>
2021-03-11 11:32:43 -0700

    Adding IsPrime stub.

---
+++ IsPrime/IsPrime/Helpers/PrimeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime.Helpers
+{
+    public static class PrimeHelper
+    {
+        public static bool IsPrime(int p)
+        {
+            return false;
+        }
+    }
+}
