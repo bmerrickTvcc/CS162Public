513a8ffc3745ab0faa319ce568612daab924690c
Brian Merrick <BrianMerrick@boisestate.edu>
2021-03-22 14:21:46 -0600

    Adding IsPrime test harness.

--- IsPrime/IsPrime/Program.cs
+++ IsPrime/IsPrime/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IsPrime.Helpers;
 
 namespace IsPrime
 {
@@ -10,6 +11,15 @@ namespace IsPrime
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine($"2: {PrimeHelper.IsPrime(2)} (should be true)");
+
+            Console.WriteLine($"61: {PrimeHelper.IsPrime(61)} (should be true)");
+
+            Console.WriteLine($"999: {PrimeHelper.IsPrime(999)} (should be false)");
+
+            Console.WriteLine($"3623: {PrimeHelper.IsPrime(3623)} (should be true)");
+
+            Console.WriteLine($"9973: {PrimeHelper.IsPrime(9973)} (should be true)");
         }
     }
 }
